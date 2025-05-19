73f9d1495a82049928925d63f5ca170cb3a9ba75
Tanvir <tanvirxng01@gmail.com>
2025-05-19 14:49:42 +0530

    change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'public' // change dist to public if needed
+    outDir: 'dist' // change dist to public if needed
   },
   server: {
     proxy: {
